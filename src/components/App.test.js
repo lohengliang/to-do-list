862ee0f7c648f25404a1ec324d506250b6d0ec26
Loh Eng Liang <lohengliang@gmail.com>
2020-02-06 18:50:44 +0800

    Added test.

---
+++ src/components/App.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from '../reducers'
+import App from './App';
+import '@testing-library/jest-dom/extend-expect';
+
+const store = createStore(rootReducer)
+
+test('renders learn react link', () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>);
+  const linkElement = getByText(/Active/i);
+  expect(linkElement).toBeInTheDocument();
+});
